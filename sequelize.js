386e0a3ce9f51d44b20046f1adcb0fc2c1ce57be
Azeem Syed <azeempsyed@gmail.com>
2019-02-24 02:00:26 +0530

    updated postgres config

--- sequelize.js
+++ sequelize.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('simple-0-dev-db','postgres', 'password', {
+const sequelize = new Sequelize('dcaqe63ohtvhsa','uoqqksjwdzzrhl', 'db123e489057e7099beeeab692e3af95e21c2a2e0e373d607dcb8d6d2cabe155', {
   dialect: 'postgres',
   operatorsAliases: false 
 });
